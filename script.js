99a92b49cfd22f7b3822b156da83680e83d2c558
ridhoarazzak <54082062+ridhoarazzak@users.noreply.github.com>
2025-06-24 23:57:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 window.onload = () => {
+  // Inisialisasi peta
   const map = L.map('map').setView([-1.5785, 101.3123], 12);
 
-  // Basemap
+  // Basemap: citra satelit Esri
   const esri = L.tileLayer(
     'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-    { attribution: '© Esri' }
+    {
+      attribution: '© Esri & contributors'
+    }
   ).addTo(map);
 
-  // Tile GEE (dari URL terbaru dan publik)
-  const geeTileUrl = "https://earthengine.googleapis.com/v1/projects/ee-mrgridhoarazzak/maps/7e20cbfd7604b78aac9a323e05b86841-f3d5ed50aef7df008eab6d7b99bebfde/tiles/{z}/{x}/{y}";
+  // Tile dari Earth Engine
+  const geeTileUrl = "https://earthengine.googleapis.com/v1/projects/ee-mrgridhoarazzak/maps/f7817adcbff8fe1ca9831d8844213532-1d36da76844ad7a1913da0a69992b318/tiles/{z}/{x}/{y}";
 
   const geeLayer = L.tileLayer(geeTileUrl, {
-    attribution: "Google Earth Engine",
-    opacity: 0.5
+    attribution: "Data: Google Earth Engine",
+    opacity: 1.0
   }).addTo(map);
 
   // Legenda
@@ -20,7 +23,7 @@ window.onload = () => {
     const div = L.DomUtil.create('div', 'legend');
     div.innerHTML = `
       <strong>Legenda</strong><br>
-      <i style="background:#0000ff"></i> Potensi Irigasi (GEE)
+      <i style="background:#0000ff"></i> Potensi Irigasi
     `;
     return div;
   }.addTo(map);
