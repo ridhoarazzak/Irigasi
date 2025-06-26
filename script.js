fdd0d164348ad9857537cdfe569cc6b83ea1ef48
ridhoarazzak <54082062+ridhoarazzak@users.noreply.github.com>
2025-06-26 16:29:53 +0700

    Update script.js
    Commit

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 window.onload = () => {
   const map = L.map('map').setView([-1.5785, 101.3123], 12);
 
-  // Basemap satelit
   const esri = L.tileLayer(
     'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
     {
@@ -9,20 +8,18 @@ window.onload = () => {
     }
   ).addTo(map);
 
-  // Tile dari Earth Engine
-  const geeTileUrl = "https://earthengine.googleapis.com/v1/projects/ee-mrgridhoarazzak/maps/f7817adcbff8fe1ca9831d8844213532-3b8a4784ed3a127a6ad5b53e149b1a03/tiles/{z}/{x}/{y}";
+  const geeTileUrl = "https://earthengine.googleapis.com/v1/projects/ee-mrgridhoarazzak/maps/7e20cbfd7604b78aac9a323e05b86841-0729c88aea7bcc419d81215e2a984b3a/tiles/{z}/{x}/{y}";
 
   const geeLayer = L.tileLayer(geeTileUrl, {
     attribution: "Data: Google Earth Engine",
     opacity: 1.0
   }).addTo(map);
 
-  // Legenda
   L.control({ position: 'bottomright' }).onAdd = () => {
     const div = L.DomUtil.create('div', 'legend');
     div.innerHTML = `
       <strong>Legenda</strong><br>
-      <i style="background:#0000ff"></i> Potensi Irigasi
+      <i></i> Potensi Irigasi
     `;
     return div;
   }.addTo(map);
